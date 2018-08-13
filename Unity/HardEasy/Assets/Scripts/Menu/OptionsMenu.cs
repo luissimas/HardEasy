f6c8304fd35ea069de5ff4a9020ed768716df10c
Luís Augusto Simas <laugustosimas@hotmail.com>
2018-08-12 22:07:41 -0300

    Adição do menu de opções
    - Adicionado um menu de opções no menu inicial, ainda necessita de alguns ajustes.
    
    - Organização das pastas dentro da Unity melhorada, assim como algumas coreções e limpeza.

---
+++ Unity/HardEasy/Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+	public Slider SliderVolumePrincipal;
+	public Slider SliderVolumeDaMusica;
+	public TMP_Text TextVolumePrincial;
+	public TMP_Text TextVolumeDaMusica;
+
+	private void Update()
+	{
+		TextVolumePrincial.text = SliderVolumePrincipal.value.ToString() + "%";
+		TextVolumeDaMusica.text = SliderVolumeDaMusica.value.ToString() + "%";
+	}
+
+	public void Redefinir()
+	{
+		SliderVolumeDaMusica.value = 50;
+		SliderVolumePrincipal.value = 50;
+	}
+
+	public void Confirmar()
+	{
+		Debug.Log("Configurações alteradas com sucesso!");
+	}
+}
